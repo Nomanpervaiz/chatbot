3d843a06ca17da4b6472817746b9ca5647c1dd76
Nomanpervaiz <nomanpervaiz035@gmail.com>
2024-05-23 03:40:57 +0500

    added files

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ let list = document.getElementById("list");
 let typing = document.getElementById("typing");
 let course = document.getElementById("course");
 let icon = document.getElementById("icon");
+let input = document.querySelector(".my-input");
+let input1 = document.querySelector(".my-input1");
+let input2 = document.querySelector(".my-input2");
 
+function lastMessage() {
+  var scrol = document.querySelector(".scroll");
+  scrol.scrollTop = scrol.scrollHeight;
+}
 function sendMessage() {
   let InpValue = event.target;
+  lastMessage();
+
   if (event.keyCode === 13) {
     list.innerHTML += `<div class="ul-div" ><li class="right-side"> ${InpValue.value} </li></div>`;
     typing.style.display = "block";
     icon.style.display = "none";
+
     if (
       ["hi", "hey", "hello", "hy", "oye", "salam", "asalam o alaikum"].indexOf(
         InpValue.value.toLowerCase()
@@ -16,11 +26,13 @@ function sendMessage() {
     ) {
       setTimeout(function abc() {
         list.innerHTML += `<li class="left-side">  Hello, Welcome to SMIT chatbot: </li>`;
+        lastMessage();
       }, 2000);
       setTimeout(function abc() {
         list.innerHTML += `<li class="left-side ">Ap kesy ho? </li>`;
         typing.style.display = "none";
         icon.style.display = "block";
+        lastMessage();
       }, 4000);
     } else if (
       [
@@ -36,9 +48,10 @@ function sendMessage() {
       ].indexOf(InpValue.value.toLowerCase()) !== -1
     ) {
       setTimeout(function abc() {
-        list.innerHTML += `<li class="left-side"> Jee batayen mai apki kya madad karskta hoon?  </li>`;
+        list.innerHTML += `<li class="left-side"> Jee batayen addmissions ke hawale se apko kya madad chahiye?  </li>`;
         typing.style.display = "none";
         icon.style.display = "block";
+        lastMessage();
       }, 2000);
     } else if (
       [
@@ -46,40 +59,173 @@ function sendMessage() {
         "mujhy addmission chahiye",
         "addmission lena hai",
         "addmission chahiye",
-        "addmission chaiye"
+        "addmission chaiye",
       ].indexOf(InpValue.value.toLowerCase()) !== -1
     ) {
       setTimeout(function abc() {
         list.innerHTML += `<li class="left-side"> Jee zaroor batyen apko konse course main addmission lena hai?
-      
         </li>`;
       }, 2000);
-    
-
       setTimeout(function abc() {
         list.innerHTML += `<li class="left-side course-btn">
-        <button class="left-side course">Web and app development</button>
-        <button class="left-side course">Python programming language</button>
-        <button class="left-side course">Gaphic designing</button>
-        <button class="left-side course">Gen Ai and Chatbot</button>
-        <button class="left-side course">3D animation</button>
-        </li>`;
+        <button class="left-side course" onclick = "webAndApp()" >Web and app development</button>
+        <button class="left-side course" onclick = "python()">Python programming language</button>
+        <button class="left-side course" onclick = "graphic()">Gaphic designing</button>
+        <button class="left-side course" onclick = "gen()">Gen Ai and chatbot</button>
+        <button class="left-side course" onclick = "animation()">3D animation</button></li>`;
         typing.style.display = "none";
+
         icon.style.display = "block";
+        lastMessage();
       }, 4000);
-      
-
-    } 
-
-
-
-    else {
+    } else {
       setTimeout(function abc() {
         list.innerHTML += `<li class="left-side"> Sorry samjh hi nahi aya ! Moye Moye 🕺🕺 </li>`;
         typing.style.display = "none";
         icon.style.display = "block";
+        lastMessage();
       }, 2000);
     }
     InpValue.value = "";
+  }
+}
+
+// courses function!
+function webAndApp() {
+  setTimeout(function abc() {
+    list.innerHTML += `<div class="ul-div" ><li class="right-side">Web and app development</li></div>`;
+    lastMessage();
+  }, 1000);
+  setTimeout(function abc() {
+    list.innerHTML += `<li class="left-side"> Apna name batayen </li>`;
+    typing.style.display = "none";
+    icon.style.display = "block";
+    input1.style.display = "block";
+    input2.style.display = "none";
+    input.style.display = "none";
+    lastMessage();
+  }, 3000);
+}
+function python() {
+  setTimeout(function abc() {
+    list.innerHTML += `<div class="ul-div" ><li class="right-side"> Python programming language </li></div>`;
+    lastMessage();
+  }, 1000);
+  setTimeout(function abc() {
+    list.innerHTML += `<li class="left-side"> Apna name batayen </li>`;
+    typing.style.display = "none";
+    icon.style.display = "block";
+    input1.style.display = "block";
+    input.style.display = "none";
+    input2.style.display = "none";
+    lastMessage();
+  }, 3000);
+}
+function gen() {
+  setTimeout(function abc() {
+    list.innerHTML += `<div class="ul-div" ><li class="right-side"> Gen Ai and chatbot </li></div>`;
+    lastMessage();
+  }, 1000);
+  setTimeout(function abc() {
+    list.innerHTML += `<li class="left-side"> Apna name batayen </li>`;
+    typing.style.display = "none";
+    icon.style.display = "block";
+    input1.style.display = "block";
+    input.style.display = "none";
+    input2.style.display = "none";
+    lastMessage();
+  }, 3000);
+}
+function graphic() {
+  setTimeout(function abc() {
+    list.innerHTML += `<div class="ul-div" ><li class="right-side"> Graphic designing </li></div>`;
+    lastMessage();
+  }, 1000);
+  setTimeout(function abc() {
+    list.innerHTML += `<li class="left-side"> Apna name batayen </li>`;
+    typing.style.display = "none";
+    icon.style.display = "block";
+    input1.style.display = "block";
+    input2.style.display = "none";
+    lastMessage();
+
+    input.style.display = "none";
+  }, 3000);
+}
+function animation() {
+  setTimeout(function abc() {
+    list.innerHTML += `<div class="ul-div" ><li class="right-side"> 3D animation</li></div>`;
+    lastMessage();
+  }, 1000);
+  setTimeout(function abc() {
+    list.innerHTML += `<li class="left-side"> Apna name batayen </li>`;
+    typing.style.display = "none";
+    input1.style.display = "block";
+    input.style.display = "none";
+    icon.style.display = "block";
+    input2.style.display = "none";
+    lastMessage();
+  }, 3000);
+}
+
+function sendName() {
+  let InpName = event.target;
+  lastMessage();
+
+  if (event.keyCode === 13) {
+    list.innerHTML += `<div class="ul-div" ><li class="right-side"> ${InpName.value} </li></div>`;
+    if (isNaN(InpName.value.toLowerCase())) {
+      setTimeout(function abc() {
+        list.innerHTML += `<li class="left-side">Theek hai ab Apne Father ka name batao. </li>`;
+        typing.style.display = "none";
+        icon.style.display = "block";
+        input1.style.display = "none";
+        input.style.display = "none";
+        input2.style.display = "block";
+        lastMessage();
+      }, 2000);
+    } else {
+      setTimeout(function abc() {
+        lastMessage();
+
+        list.innerHTML += `<li class="left-side"> Number koi name nhi huta </li>`;
+        typing.style.display = "none";
+        icon.style.display = "block";
+        lastMessage();
+      }, 2000);
+    }
+
+    InpName.value = "";
+  }
+}
+function sendFatherName() {
+  let InpFatherName = event.target;
+  lastMessage();
+
+  if (event.keyCode === 13) {
+    list.innerHTML += `<div class="ul-div" ><li class="right-side"> ${InpFatherName.value} </li></div>`;
+    if (isNaN(InpFatherName.value.toLowerCase())) {
+      setTimeout(function abc() {
+        list.innerHTML += `<li class="left-side">Theek hai ab Apne CNIC number humayen bejain.</li>`;
+        typing.style.display = "none";
+        icon.style.display = "block";
+        input.style.display = "none";
+        input1.style.display = "none";
+        input2.style.display = "block";
+        lastMessage();
+      }, 2000);
+    } else if (!isNaN(InpFatherName.value)) {
+      setTimeout(function abc() {
+        list.innerHTML += `<li class="left-side">Theek hai shukriya CNIC ke liye humare addmission abi close hai</li>`;
+        typing.style.display = "none";
+        icon.style.display = "block";
+        input.style.display = "none";
+        input1.style.display = "none";
+        input2.style.display = "block";
+        lastMessage();
+      }, 2000);
+    }
+
+    InpFatherName.value = "";
   }
 }
